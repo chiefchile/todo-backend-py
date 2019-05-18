54be595d987ae4fc719c2636f4b4fe8c227c49ef
alex <chiefchile@gmail.com>
2019-05-18 16:05:37 +0800

    Handle note not found

--- todo_backend_py/note/views.py
+++ todo_backend_py/note/views.py
@@ -12,7 +12,11 @@ from todo_backend_py.common.models import Result
 class NoteView(APIView):
 	
 	def get(self, request, pk, format=None):
-		note = Note.objects.get(pk=pk)
+		try:
+			note = Note.objects.get(pk=pk)
+		except Note.DoesNotExist:
+			return Response(vars(Result(code=-1, msg="Not found")))
+		
 		serializer = NoteSerializer(note)
 		result = GetNoteResult(code=0, msg="Success", note=serializer.data)
 		return Response(vars(result))
