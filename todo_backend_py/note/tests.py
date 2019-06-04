00c9a9ebcc80b373f0e8629c3a27cbaadf72d240
alex <aa26_ph@yahoo.com>
2019-06-04 15:45:09 +0800

    Add more tests

--- todo_backend_py/note/tests.py
+++ todo_backend_py/note/tests.py
@@ -1,33 +1,78 @@
-from builtins import classmethod
-
 from django.contrib.auth.models import User
 from rest_framework import status
 from rest_framework.test import APITestCase
 
-from todo_backend_py.note.models import Note
+TITLE = "world"
+
+NOTE = "hello"
 
 
 class NoteTests(APITestCase):
 
-    # @classmethod
     def setUp(self):
         self.user = User.objects.create_user("testuser", password="testuser")
         self.client.post('/api-token-auth/', {"username": "testuser", "password": "testuser"},
                                           format='json')
+        self.client.force_authenticate(user=self.user, token=self.user.auth_token)
 
     def create_note(self):
-        self.client.force_authenticate(user=self.user, token=self.user.auth_token)
-        return self.client.post('/note/', {"note": "note", "title": "title"},
-                                    format='json')
+        return self.client.post('/note/', {"note": NOTE, "title": TITLE},
+                                format='json')
 
     def test_get_note(self):
-        response = self.create_note()
-        self.client.force_authenticate(user=self.user, token=self.user.auth_token)
-        url = f"/note/{response.data['_id']}/"
-        response = self.client.get(url)
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        create_response = self.create_note()
+        url = f"/note/{create_response.data['_id']}/"
+
+        get_response = self.client.get(url)
+
+        self.assertEqual(get_response.status_code, status.HTTP_200_OK)
+        self.check_note(get_response, NOTE, TITLE)
+
+    def check_note(self, response, expected_note, expected_title):
+        self.assertEqual(response.data["note"], expected_note)
+        self.assertEqual(response.data["title"], expected_title)
 
     def test_create_note(self):
-        self.client.force_authenticate(user=self.user, token=self.user.auth_token)
         response = self.create_note()
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+    def test_update_note(self):
+        create_response = self.create_note()
+        url = f"/note/{create_response.data['_id']}/"
+        updated_note = "updated note"
+        updated_title = "updated title"
+
+        response = self.client.put(url, {"note": updated_note, "title": updated_title},
+                                   format='json')
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.check_note(response, updated_note, updated_title)
+
+    def test_delete_note(self):
+        create_response = self.create_note()
+        url = f"/note/{create_response.data['_id']}/"
+
+        response = self.client.delete(url)
+
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+
+    def test_get_titles(self):
+        self.create_note()
+
+        response = self.client.get("/titles/")
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertContains(response, TITLE)
+
+    def test_delete_test_data(self):
+        self.create_note()
+
+        response = self.client.get("/note/deleteTestData/")
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+
+
+
+
+
